1a6a0c00bb831f46c3b88ec5a679520681d89599
SachaBate <31642615+sachabate@users.noreply.github.com>
2023-04-20 10:19:42 -0600

    Initial Commit

---
+++ Hoodoo/HoodooApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoodooApp.swift
+//  Hoodoo
+//
+//  Created by Alex Bate on 2023-04-20.
+//
+
+import SwiftUI
+
+@main
+struct HoodooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
